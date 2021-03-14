b0154f8c3e408d3950abf7676f59f02bd0ace746
bambi teague <teeegsa@gmail.com>
2021-03-14 12:44:32 -0500

    commit tree 071a6951e70fd94aa9122e51f241a1661a179b66

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,9 +15,10 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.string :size
+      t.string  :size
       t.string :image_url
-      t.timestamps
+
+      t.timestamps null: false
     end
   end
 end
